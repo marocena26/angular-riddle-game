2a1509689f296bbea258055d850037a515392d2f
Elena Arocena <mariaelena.arocena@gmail.com>
2024-03-25 16:52:50 +0100

    feat(home): add structure for card component

--- src/app/modules/pages/home/home.component.ts
+++ src/app/modules/pages/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { QuestionsAndAnswers } from 'src/app/interfaces/questionsAndAnswers';
 
 @Component({
   selector: 'app-home',
@@ -6,5 +7,41 @@ import { Component } from '@angular/core';
   styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent {
+
+  currentIndex: number = 0;
+  userAnswer: string = '';
+  responseMessage: string = '';
+
+  questions: QuestionsAndAnswers[] = [
+    {
+      question:
+        '¿Qué lenguaje de programación fue creado por Guido van Rossum?',
+      answers: 'Python',
+    },
+
+    {
+      question: '¿Cuál es el sistema operativo de código abierto más popular?',
+      answers: 'Linux',
+    },
+  ];
+
+
+  //Comprobamos el índice de la pregunta actual
+  getCurrentQuestion(): QuestionsAndAnswers {
+    return this.questions[this.currentIndex];
+  }
+
+
+  //Comprobamos si la respues es correcta o incorrecta
+  checkAnswer(): void {
+    const currentQuestion = this.getCurrentQuestion();
+    if (this.userAnswer.trim().toLowerCase() === currentQuestion.answers.toLowerCase()) {
+      this.currentIndex++;
+      this.userAnswer = '';
+      this.responseMessage = '';
+    } else {
+
+      this.responseMessage = 'Respuesta incorrecta. Inténtalo de nuevo.';    }
+  }
 
 }
